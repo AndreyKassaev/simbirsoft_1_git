6a4fe46ee2ef0d86d1a2da0fad0fb809ddf47b8f
andrey kassaev <andrey@kassaev.com>
2025-03-03 13:03:35 +0400

    Add ui and unit tests

--- feature_help/src/androidTest/java/com/kassaev/simbirsoft_1_git/feature/help/ExampleInstrumentedTest.kt
+++ feature_help/src/androidTest/java/com/kassaev/simbirsoft_1_git/feature/help/ExampleInstrumentedTest.kt
@@ -1,24 +1,24 @@
 package com.kassaev.simbirsoft_1_git.feature.help
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import com.kassaev.simbirsoft_1_git.feature.help.screen.HelpScreen
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    val composeRule = createComposeRule()
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.kassaev.simbirsoft_1_git.feature.help.test", appContext.packageName)
+    fun simpleUiTest() {
+        composeRule.setContent {
+            HelpScreen()
+        }
+        composeRule.onNodeWithTag("helpScreen").assertExists()
     }
 }
